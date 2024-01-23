a3ce235269cc0addf72cd2d8281355b3f130c2bc
najum <najumtamayo@gmail.com>
2024-01-23 18:16:30 +0100

    Añadido método 2

--- src/main/java/eus/ehu/eui/isoft/Main.java
+++ src/main/java/eus/ehu/eui/isoft/Main.java
@@ -1,7 +1,16 @@
 package eus.ehu.eui.isoft;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+    }
+
+    public static void Method1(){
+        System.out.println("Esto es un método");
+    }
+
+    public static void Method2(){
+        System.out.println("Esto es otro método");
     }
 }
